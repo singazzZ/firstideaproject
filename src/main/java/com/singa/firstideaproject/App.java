dfc03cc011712a115a995c61124a22cf592fd368
singazzZ <singa123@qq.com>
2019-01-21 17:26:50 +0800

    my first idea project

---
+++ src/main/java/com/singa/firstideaproject/App.java
@@ -0,0 +1,21 @@
+package com.singa.firstideaproject;
+
+public class App {
+
+    public static final String s = null;
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+
+    }
+
+    /**
+     * @Description
+     * @Date 17:23 2019/1/21
+     * @Param []
+     * @return void
+     */
+    public void test() {
+        System.out.println(123);
+    }
+}
---
+++ src/main/java/com/singa/firstideaproject/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.singa.firstideaproject;
+
+/**
+ * @Author Singa
+ * @Version V1.0.0
+ * @Description TODO
+ * @Date 2019年01月21日 17:25
+ * @Copyright(C) Singa
+ */
+public class HelloWorld {
+}
